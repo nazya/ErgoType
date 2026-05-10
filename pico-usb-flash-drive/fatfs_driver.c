795709c6cd39f1e848d4e7cbafe92e3d93609429
nazya <two_nazya@mail.ru>
2026-05-10 16:01:48 +0400

    flash: fix fatfs backend semantics

--- pico-usb-flash-drive/fatfs_driver.c
+++ pico-usb-flash-drive/fatfs_driver.c
@@ -9,40 +9,18 @@
 static DSTATUS Stat = RES_OK; // STA_NOINIT;
 
 
-typedef struct {
-  uint8_t DIR_Name[11];
-  uint8_t DIR_Attr;
-  uint8_t DIR_NTRes;
-  uint8_t DIR_CrtTimeTenth;
-  uint16_t DIR_CrtTime;
-  uint16_t DIR_CrtDate;
-  uint16_t DIR_LstAccDate;
-  uint16_t DIR_FstClusHI;
-  uint16_t DIR_WrtTime;
-  uint16_t DIR_WrtDate;
-  uint16_t DIR_FstClusLO;
-  uint32_t DIR_FileSize;
-} fat_dir_entry_t;
-
-
 DSTATUS disk_status(BYTE drv) {
+    (void)drv;
     return Stat;
 }
 
 DSTATUS disk_initialize(BYTE drv) {
+    (void)drv;
     uint8_t block[FAT_BLOCK_SIZE];
     flash_fat_read(0, block);
 
     uint16_t magic = block[FAT_BLOCK_SIZE - 2] << 8 | block[FAT_BLOCK_SIZE - 1];
     if (magic == FAT_MAGIC) {
-        Stat = RES_OK;
-        return Stat;
-    }
-
-    flash_fat_read(2, block);
-    fat_dir_entry_t *dir = (fat_dir_entry_t *)block;
-    dir++;
-    if (strcmp(dir->DIR_Name, "README  TXT") == 0) {
         Stat = RES_OK;
         return Stat;
     }
@@ -55,20 +33,46 @@ DSTATUS disk_initialize(BYTE drv) {
 }
 
 DRESULT disk_read(BYTE drv, BYTE *buff, LBA_t sector, UINT count) {
-    if (sector > FAT_BLOCK_NUM) {
-        return RES_ERROR;
+    (void)drv;
+    if (sector >= FAT_BLOCK_NUM || (sector + count) > FAT_BLOCK_NUM) {
+        return RES_PARERR;
     }
-    flash_fat_read(sector, (uint8_t *)buff);
+    for (UINT i = 0; i < count; ++i) {
+        flash_fat_read((int)(sector + i), (uint8_t *)(buff + (i * FAT_BLOCK_SIZE)));
+    }
     return RES_OK;
 }
 
 DRESULT disk_write(BYTE drv, const BYTE *buff, LBA_t sector, UINT count) {
-    flash_fat_write(sector, (uint8_t *)buff);
+    (void)drv;
+    if (sector >= FAT_BLOCK_NUM || (sector + count) > FAT_BLOCK_NUM) {
+        return RES_PARERR;
+    }
+    for (UINT i = 0; i < count; ++i) {
+        if (!flash_fat_write((int)(sector + i), (uint8_t *)(buff + (i * FAT_BLOCK_SIZE)))) {
+            return RES_ERROR;
+        }
+    }
     return RES_OK;
 }
 
 DRESULT disk_ioctl(BYTE drv, BYTE ctrl, void *buff) {
-    return RES_OK;
+    (void)drv;
+    switch (ctrl) {
+    case CTRL_SYNC:
+        return RES_OK;
+    case GET_SECTOR_COUNT:
+        *(LBA_t *)buff = FAT_BLOCK_NUM;
+        return RES_OK;
+    case GET_SECTOR_SIZE:
+        *(WORD *)buff = FAT_BLOCK_SIZE;
+        return RES_OK;
+    case GET_BLOCK_SIZE:
+        *(DWORD *)buff = (DWORD)(FLASH_SECTOR_SIZE / FAT_BLOCK_SIZE);
+        return RES_OK;
+    default:
+        return RES_PARERR;
+    }
 }
 
 DWORD get_fattime (void) {
