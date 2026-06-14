1063963870f2076327018b945fdf01faa187b96d
nazya <two_nazya@mail.ru>
2026-06-14 22:21:54 +0400

    keyd: devices

--- keyd/port/device.h
+++ keyd/port/device.h
@@ -6,7 +6,28 @@
 #ifndef DEVICE_H
 #define DEVICE_H
 
+#include <stddef.h>
 #include <stdint.h>
+
+#include "FreeRTOS.h"
+#include "queue.h"
+
+#define MAX_DEVICES 8
+#define DEVICE_EVENT_QUEUE_LEN 16
+#define DEVICE_EVENT_SET_LEN (MAX_DEVICES * DEVICE_EVENT_QUEUE_LEN + MAX_DEVICES)
+
+#define CAP_MOUSE	0x1
+#define CAP_MOUSE_ABS	0x2
+#define CAP_KEYBOARD	0x4
+#define CAP_KEY		0x8
+
+struct device {
+	QueueHandle_t events;
+	uint8_t capabilities;
+	const char *id;
+	const char *name;
+	void *data;
+};
 
 struct device_event {
 	enum {
@@ -18,7 +39,7 @@ struct device_event {
 		DEV_MOUSE_MOVE_ABS,
 		DEV_MOUSE_SCROLL,
 
-		// DEV_REMOVED,
+		DEV_REMOVED,
 	} type;
 
 	uint8_t code;
@@ -28,5 +49,14 @@ struct device_event {
 	int32_t y;
 };
 
-#endif
+extern QueueHandle_t devmon_queue;
+extern struct device device_table[MAX_DEVICES];
+extern size_t device_table_sz;
 
+void devmon_init(void);
+void device_add(const struct device *dev);
+void device_delete(struct device *dev);
+struct device_event *device_read_event(struct device *dev);
+QueueSetMemberHandle_t device_select(int timeout);
+
+#endif
