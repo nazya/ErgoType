1ff3b4563bf3728a812393ff45b35a7a4ff30bf6
nazya <two_nazya@mail.ru>
2026-07-23 00:30:30 +0400

    webhid: reset interrupted transactions

--- tusb/usb_webhid.h
+++ tusb/usb_webhid.h
@@ -7,6 +7,7 @@
 #include "tusb.h"
 
 bool webhid_is_instance(uint8_t instance);
+void webhid_reset(void);
 uint16_t webhid_get_report(uint8_t report_id, hid_report_type_t report_type,
                            uint8_t* buffer, uint16_t reqlen);
 void webhid_set_report(uint8_t report_id, hid_report_type_t report_type,
